34f87489c576336eb33655039ee4f3c508224547
anonymous805500 <mm2452@kent.ac.uk>
2024-10-16 11:33:48 +0000

    2.1

--- PartTwo.py
+++ PartTwo.py
@@ -2,6 +2,7 @@
 x = 0
 while x < 75:
     pay = int(input("Please insert coins "))
-    
+    amount_due = 75 - pay
+    print(amount_due , "pence")
     x = x + pay
 print("change due:" , x - 75)    
